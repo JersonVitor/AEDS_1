a381d74b4e18e45258c37dde3558cbe9e2efe89f
JeronVitor <jersonvitor2016@gmail.com>
2023-07-20 12:49:50 -0300

    Update

---
+++ Listas/Lista_00/Questao1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	puts("\nPerimetro do quadrado de lado 7\n");
+
+	int x = 7;
+
+	int perimetro = x * 4;
+
+	printf("O perimetro e: %d\n", perimetro);
+}
